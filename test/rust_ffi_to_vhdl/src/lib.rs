d7fb1c9588be671f441b0b964926c487cf0f8512
marcantoinem <marc-antoine.m@outlook.com>
2024-05-19 19:20:18 -0400

    Forbidden FFI with Rust

--- test/rust_ffi_to_vhdl/src/lib.rs
+++ test/rust_ffi_to_vhdl/src/lib.rs
@@ -4,11 +4,6 @@ use once_cell::sync::Lazy;
 static TCP: Lazy<TcpListener> = Lazy::new(|| TcpListener::bind("127.0.0.1:3000").unwrap());
 
 #[no_mangle]
-pub unsafe extern "C" fn what_have_i_done() {
-    println!("What have I done!");
-}
-
-#[no_mangle]
 pub unsafe extern "C" fn respond_tcp_request() {
     let Ok((mut stream, _addr)) = TCP.accept() else {return};
     let _ = stream.write_all(b"HTTP/1.1 200 OK\r\nContent-Length: 60\r\n\r\n<!DOCTYPE html>
--- test/src/foreign_interface/udp.vhd
+++ test/src/foreign_interface/udp.vhd
@@ -1,14 +1,9 @@
 package udp is
-    procedure what_have_i_done;
-    attribute foreign of what_have_i_done : procedure is "VHPIDIRECT what_have_i_done";  
-
     procedure respond_tcp_request;
     attribute foreign of respond_tcp_request : procedure is "VHPIDIRECT respond_tcp_request";  
 end package udp;
 
 package body udp is 
-    procedure what_have_i_done is begin
-    end procedure;
     procedure respond_tcp_request is begin
     end procedure;
 end package body;
